8792aebadb47dafdc8e6a7d438566e5fc33d975d
Itati26 <reyesitati6@gmail.com>
2026-06-19 07:33:47 -0600

    Descripción breve de los cambios realizados

--- auth.php
+++ auth.php
@@ -1,15 +1,12 @@
 <?php
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
-header("Content-Type: application/json");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Credentials: true");
 
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') { 
-    http_response_code(200); 
-    exit; 
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit;
 }
 
 require "conexion.php";
